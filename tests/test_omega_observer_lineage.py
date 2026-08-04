f9e06b3abac8a7891afd7f7b3d66079d0998e2a6
echoframe12-ui <echoframe12@gmail.com>
2026-08-04 16:07:13 +0000

    fix: resolve observer lineage type hints in test

--- tests/test_omega_observer_lineage.py
+++ tests/test_omega_observer_lineage.py
@@ -1,5 +1,7 @@
+from typing import get_type_hints
+
 from omega_observer_lineage import ObserverLineage
 
 
 def test_observer_lineage_module_contract_is_importable():
-    assert ObserverLineage.__annotations__["lineage_depth"] is int
+    assert get_type_hints(ObserverLineage)["lineage_depth"] is int
